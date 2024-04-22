b278b5ac80d270db0b37a71617c36d716e23e6d6
Flávio <f.augustosdn@gmail.com>
2024-04-22 20:55:53 -0300

    adicionado validações na regra de negócio

--- src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -1,4 +1,5 @@
-﻿using CashFlow.Communication.Requests;
+﻿using CashFlow.Communication.Enums;
+using CashFlow.Communication.Requests;
 using CashFlow.Communication.Responses;
 
 namespace CashFlow.Application.UseCases.Expenses.Register;
@@ -7,8 +8,34 @@ public class RegisterExpenseUseCase
 {
     public ResponseRegisteredExpenseJson Execute(RequestExpenseJson request)
     {
-        // Validations
+        Validate(request);
 
         return new ResponseRegisteredExpenseJson();
+    }
+
+    private void Validate(RequestExpenseJson request)
+    {
+        var titleEmpty = string.IsNullOrWhiteSpace(request.Title);
+        if(titleEmpty)
+        {
+            throw new ArgumentException("The title is required.");
+        }
+
+        if(request.Amount <= 0)
+        {
+            throw new ArgumentException("The amount must be greater than zero.");
+        }
+
+        var result = DateTime.Compare(request.Date, DateTime.UtcNow);
+        if (result > 0)
+        {
+            throw new ArgumentException("Expenses cannot be for the future.");
+        }
+
+        var paymentTypeIsValid = Enum.IsDefined(typeof(PaymentType), request.PaymentType);
+        if(!paymentTypeIsValid)
+        {
+            throw new ArgumentException("Payment type is not valid.");
+        }
     }
 }
